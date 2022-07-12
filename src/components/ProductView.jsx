d63e7452f2e4895651276734b4b6049ed5931e3b
Q-Duong <huynhquocduong12345@gmail.com>
2022-07-12 12:29:53 +0700

    fix css

--- src/components/ProductView.jsx
+++ src/components/ProductView.jsx
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { withRouter } from 'react-router'
-
+import { Link } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 
 import { addItem } from '../redux/shopping-cart/cartItemsSlide'
 import { remove } from '../redux/product-modal/productModalSlice'
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 import Button from './Button'
 import numberWithCommas from '../utils/numberWithCommas'
-import { Col } from 'react-bootstrap'
+
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 
 const ProductView = props => {
 
@@ -53,113 +55,160 @@ const ProductView = props => {
 
     return (
         product ? 
-        <div className="product">
-            <div className="product__images">
-                <div className="product__images__main">
-                    <img src={`${process.env.REACT_APP_IMAGEURL}${previewImg}`} alt="" />
-                </div>
-                <div className="product__images__list">
-                    <div className="product__images__list__item" onClick={() => setPreviewImg(product.product.image)}>
-                        <img src={`${process.env.REACT_APP_IMAGEURL}${product.product.image}`} alt="" />
-                    </div>
-                    <div className="product__images__list__item" onClick={() => setPreviewImg(product.product.image)}>
-                        <img src={`${process.env.REACT_APP_IMAGEURL}${product.product.image}`} alt="" />
-                    </div>
-                </div>
-                
-                <div className={`product-description ${descriptionExpand ? 'expand' : ''}`}>
-                    <div className="product-description__title">
-                        Chi tiết sản phẩm
-                    </div>
-                    <div className="product-description__content" dangerouslySetInnerHTML={{__html: product.product.name}}></div>
-                    <div className="product-description__toggle">
-                        <Button size="sm" onClick={() => setDescriptionExpand(!descriptionExpand)}>
-                            {
-                                descriptionExpand ? 'Thu gọn' : 'Xem thêm'
-                            }
-                        </Button>
-                    </div>
-                </div>
-            </div>
-            <div className="product__info">
-                <h1 className="product__info__title">{product.product.name}</h1>
-                <div className="product__info__item">
-                    <span className="product__info__item__price">
-                        {numberWithCommas(product.soldPrice )}
-                    </span>
-                </div>
-                {/* <div className="product__info__item">
-                    <div className="product__info__item__title">
-                        Màu sắc
-                    </div>
-                    <div className="product__info__item__list">
-                        {
-                            product.colors.map((item, index) => (
-                                <div key={index} className={`product__info__item__list__item ${color === item ? 'active' : ''}`} onClick={() => setColor(item)}>
-                                    <div className={`circle bg-${item}`}></div>
-                                </div>
-                            ))
-                        }
-                    </div>
-                </div>
-                <div className="product__info__item">
-                    <div className="product__info__item__title">
-                        Kích cỡ
-                    </div>
-                    <div className="product__info__item__list">
-                        {
-                            product.size.map((item, index) => (
-                                <div key={index} className={`product__info__item__list__item ${size === item ? 'active' : ''}`} onClick={() => setSize(item)}>
-                                    <span className="product__info__item__list__item__size">
-                                        {item}
-                                    </span>
-                                </div>
-                            ))
-                        }
-                    </div>
-                </div> */}
-                <div className="product__info__item">
-                    <div className="product__info__item__quantity">
-                        <div className="product__info__item__title">
-                            Số lượng ({product.product.unit})
+        <section class="shop-details">
+            <div class="product__details__pic">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="product__details__breadcrumb">
+                                <Link to="/">Trang chủ</Link>
+                                <FontAwesomeIcon icon={faAngleRight} className="faAngleRight" />
+                                <a href="{{url('/product-list/'.$cate_slug)}}">thit</a>
+                                {/* <span>{{$meta_title}}</span> */}
+                            </div>
                         </div>
-                        <div className="product__info__item__quantity">
-                            <div className="product__info__item__quantity__btn" onClick={() => updateQuantity('minus')}>
-                                <i className="bx bx-minus"></i>
-                            </div>
-                            <div className="product__info__item__quantity__input">
-                                {quantity}
-                            </div>
-                            <div className="product__info__item__quantity__btn" onClick={() => updateQuantity('plus')}>
-                                <i className="bx bx-plus"></i>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-7 col-md-7">
+                        <img src={`${process.env.REACT_APP_IMAGEURL}${previewImg}`} alt="" />
+                        </div>
+                        <div class="col-lg-1 d-none d-lg-block"> </div>
+                        <div class="col-lg-4 col-md-5">
+                            <div class="product__details__text">
+                                <h3>{product.product.name}</h3>
+                                <div class="rating ">
+                                    <div class="row d-flex ">
+                                        {/* <ul class="list-inline">
+                                            @for($count=1; $count<=5; $count++) @php if($count<=$rating){
+                                                $color='color:#ffcc00;' ; } else { $color='color:#ccc;' ; } @endphp <li
+                                                title="star_rating" data-product_id="{{$value->product_id}}"
+                                                data-rating="{{$rating}}" class="rating"
+                                                style="cursor:pointer; {{$color}} font-size:30px;">&#9733;</li>
+                                                @endfor
+                                        </ul> */}
+                                    </div>
+                                </div>
+                                <h5>{numberWithCommas(product.soldPrice )}</h5>
+                                <span>Danh mục:</span> fdg
+
+                                <div className="product__info__item">
+                                    <div className="product__info__item__quantity">
+                                        <div className="product__info__item__title">
+                                            Số lượng ({product.product.unit})
+                                        </div>
+                                        <div className="product__info__item__quantity">
+                                            <div className="product__info__item__quantity__btn" onClick={() => updateQuantity('minus')}>
+                                                <i className="bx bx-minus"></i>
+                                            </div>
+                                            <div className="product__info__item__quantity__input">
+                                                {quantity}
+                                            </div>
+                                            <div className="product__info__item__quantity__btn" onClick={() => updateQuantity('plus')}>
+                                                <i className="bx bx-plus"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                    
+                                <div class="product__info__item"> 
+                                    <Button  onClick={() => {addToCart()}}>thêm vào giỏ</Button>
+                                </div>
+                                <div class="product__info__item">
+                                    <Button  onClick={() => {addToCart()}}>Yêu thích</Button>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="product__info__item">
-                    <Col>
-                        <Button  onClick={() => {addToCart()}}>thêm vào giỏ</Button>
-                    </Col>
-                    <Col>
-                        {/* <Button onClick={() => {goToCart()}}>mua ngay</Button> */}
-                        <Button  onClick={() => {addToCart()}}>Yêu thích</Button>
-                    </Col>
+            </div>
+            
+            <div class="product__details__content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="product__details__tab">
+                                <ul class="nav nav-tabs" role="tablist">
+                                    <li class="nav-item">
+                                        <a class="nav-link active" data-toggle="tab" href="#tabs-5" role="tab">Thông số kỹ
+                                            thuật</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" data-toggle="tab" href="#tabs-6" role="tab">Đặc điểm nổi bật</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" data-toggle="tab" href="#tabs-7" role="tab">Đánh giá(5)</a>
+                                    </li>
+
+                                </ul>
+                                <div class="tab-content">
+                                    <div class="tab-pane active" id="tabs-5" role="tabpanel">
+                                        <div class="product__details__tab__content">
+                                            <p class="note">
+                                            { ReactHtmlParser(product.product.description) }</p>
+                                        </div>
+                                    </div>
+                                    <div class="tab-pane" id="tabs-6" role="tabpanel">
+                                        <div class="product__details__tab__content">
+                                            <p class="note"></p>
+                                            <div class="product__details__tab__content__item">
+                                                <p>dsfhj</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="tab-pane" id="tabs-7" role="tabpanel">
+                                        <div class="product__details__tab__content">
+                                            <form>
+                                                
+                                                <input type="hidden" name="comment_product_id" class="comment_product_id"
+                                                    value="{{$value->product_id}}"/>
+                                                <div id="comment_show"></div>
+
+                                            </form>
+
+                                            <div class="h-30 row d-flex justify-content-center">
+                                                <div class="col-lg-8 centered">
+                                                    <div class="blog__details__comment">
+                                                        <h4>Thêm bình luận</h4>
+                                                        
+                                                        <h3>Đánh giá & nhận xét</h3>
+                                                        {/* <ul class="list-inline rating" title="Average Rating">
+                                                            @for($count=1; $count<=5; $count++) @php if($count<=$rating){
+                                                                $color='color:#ffcc00;' ; } else { $color='color:#ccc;' ; }
+                                                                @endphp <li title="star_rating"
+                                                                id="{{$value->product_id}}-{{$count}}" data-index="{{$count}}"
+                                                                data-product_id="{{$value->product_id}}"
+                                                                data-rating="{{$rating}}" class="rating"
+                                                                style="cursor:pointer; {{$color}} font-size:30px;">&#9733;</li>
+                                                                @endfor
+                                                        </ul> */}
+
+                                                        <form action="#">
+                                                            <div id="notify_comment"></div>
+                                                            <div class="row">
+                                                                <div class="col-lg-5 col-md-5">
+                                                                    <input type="text" class="comment_name" placeholder="Name"/>
+                                                                </div>
+                                                                <div class="col-lg-12 text-center">
+                                                                    <textarea class="comment_content"
+                                                                        placeholder="Comment"></textarea>
+                                                                    <button type="submit" class="site-btn send-comment">Gửi đánh
+                                                                        giá</button>
+                                                                </div>
+                                                            </div>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className={`product-description mobile ${descriptionExpand ? 'expand' : ''}`}>
-                <div className="product-description__title">
-                    Chi tiết sản phẩm
-                </div>
-                <div className="product-description__content" dangerouslySetInnerHTML={{__html: product.description}}></div>
-                <div className="product-description__toggle">
-                    <Button size="sm" onClick={() => setDescriptionExpand(!descriptionExpand)}>
-                        {
-                            descriptionExpand ? 'Thu gọn' : 'Xem thêm'
-                        }
-                    </Button>
-                </div>
-            </div>
-        </div>
+        </section>
         : 
         <div>loading</div>
     )
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,9 +1,12 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import { Button } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 
-import {categoryAPI, wareHouseAPI} from '../api/api';
+import {brandAPI, categoryAPI, wareHouseAPI} from '../api/api';
+import CheckBox from '../components/CheckBox';
 import Helmet from '../components/Helmet'
 import InfinityList from '../components/InfinityList'
+import ListProduct from '../components/ListProduct';
 
 
 
@@ -13,6 +16,17 @@ const Catogory = () => {
 
     const [categories, setCategories] = useState([])
 
+    const initFilter = {
+        brands: []
+    }
+
+    const [productFilter, setProductFilter]= useState([])
+
+    const [filter, setFilter] = useState(initFilter)
+
+    const [brands, setBrands] = useState([])
+
+    
 
     useEffect(() => {
         async function getCategories() {
@@ -30,6 +44,66 @@ const Catogory = () => {
         }
         getCategories()
     },[])
+
+
+
+    useEffect(() => {
+        async function getBrands() {
+            try {
+                const response = await brandAPI.getAll();
+                if(response.status === 200) {
+                    const brands = response.data
+                    setBrands(brands)
+                } else {
+                    console.log(response)
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getBrands()
+    },[])
+
+    const filterSelect = (type, checked, item) => {
+        if (checked) {
+            switch(type) {
+                case "BRAND":
+                    setFilter({...filter, brands: [...filter.brands, item._id]})
+                    break
+                default:
+            }
+        } else {
+            switch(type) {
+                case "BRAND":
+                        const newBrand = filter.brands.filter(e => e !== item._id)
+                        setFilter({...filter, brands: newBrand})
+                        break
+                default:
+            }
+        }
+    }
+
+    const clearFilter = () => setFilter(initFilter)
+
+    useEffect(
+        () => {
+            try {
+                let temp = [...products]
+
+                if (filter.brands.length > 0) {
+                    temp = temp.filter(e => filter.brands.includes(e.product.brand))
+                }
+                setProductFilter(temp)
+            } catch(err) {} 
+        },
+        [filter]
+    )
+
+    const filterRef = useRef(null)
+
+    const showHideFilter = () => filterRef.current.classList.toggle('active')
+
+
 
     useEffect(() => {
         async function getProducts() {
@@ -52,9 +126,41 @@ const Catogory = () => {
     return (
         <Helmet title="Sản phẩm">
             <div className="catalog">
+                <div className="catalog__filter" ref={filterRef}>
+                    <div className="catalog__filter__close" onClick={() => showHideFilter()}>
+                        <i className="bx bx-left-arrow-alt"></i>
+                    </div>
+
+                    <div className="catalog__filter__widget">
+                        <div className="catalog__filter__widget__title">
+                            Hãng
+                        </div>
+                        <div className="catalog__filter__widget__content">
+                            {
+                                brands.map((item) => (
+                                    <div key={item._id} className="catalog__filter__widget__content__item">
+                                        <CheckBox
+                                            label={item.name}
+                                            onChange={(input) => filterSelect("BRAND", input.checked, item)}
+                                            checked={filter.brands.includes(item._id)}
+                                        />
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+
+                    <div className="catalog__filter__widget">
+                        <div className="catalog__filter__widget__content">
+                            <Button size="sm" onClick={clearFilter}>xóa bộ lọc</Button>
+                        </div>
+                    </div>
+                </div>
+                <div className="catalog__filter__toggle">
+                    <Button size="sm" onClick={() => showHideFilter()}>bộ lọc</Button>
+                </div>
                 <div className="catalog__content">
-                    
-                    <InfinityList
+                    <ListProduct
                         products={products}
                     />
                 </div>
